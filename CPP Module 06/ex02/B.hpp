19f5002c72492161784680b737dfbd5a9aa43584
Benedikt Hielscher <bhielsch@c1r1p4.42vienna.com>
2023-09-28 12:09:37 +0200

    ex02

---
+++ CPP Module 06/ex02/B.hpp
@@ -0,0 +1,10 @@
+#ifned B_HPP
+#define B_HPP
+
+#include "Base.hpp"
+
+class B: public Base {
+
+};
+
+#endif
---
+++ CPP Module 06/ex02/C.hpp
@@ -0,0 +1,10 @@
+#ifned C_HPP
+#define C_HPP
+
+#include "Base.hpp"
+
+class C: public Base {
+
+};
+
+#endif
